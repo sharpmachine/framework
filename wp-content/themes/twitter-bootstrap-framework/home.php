e1cf6264068c081fc75db0bc90a8d671b1343466
Jesse Kade <jessekade@gmail.com>
2012-05-24 13:50:57 -0700

    twitter bootstrap framework is coming along nicely

---
+++ wp-content/themes/twitter-bootstrap-framework/home.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+	
+		<section id="page" class="span8">
+			
+			<h1>Bootstrap starter template</h1>
+		    <p>Use this document as a way to quick start any new project.<br> All you get is this message and a barebones HTML document.</p>
+		
+			<?php get_template_part( 'loop', 'index' ); ?>
+		
+		</section><!-- #page -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
